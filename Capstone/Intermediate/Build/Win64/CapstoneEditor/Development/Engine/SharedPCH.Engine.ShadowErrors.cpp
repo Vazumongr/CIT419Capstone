1a69cf64c11ecf77bc1ecb0d5d7aea42cec9ad33
Troy Records Jr <giants2110@gmail.com>
2020-08-17 14:53:58 -0400

    Setup the UE Project

---
+++ Capstone/Intermediate/Build/Win64/CapstoneEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/troy/Documents/Repositories/Githubslabs/CIT419Capstone/CIT419Capstone/Capstone/Intermediate/Build/Win64/CapstoneEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Capstone/Intermediate/Build/Win64/UE4Editor/Inc/Capstone/CapstoneGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CAPSTONE_CapstoneGameModeBase_generated_h
+#error "CapstoneGameModeBase.generated.h already included, missing '#pragma once' in CapstoneGameModeBase.h"
+#endif
+#define CAPSTONE_CapstoneGameModeBase_generated_h
+
+#define Capstone_Source_Capstone_CapstoneGameModeBase_h_15_SPARSE_DATA
+#define Capstone_Source_Capstone_CapstoneGameModeBase_h_15_RPC_WRAPPERS
+#define Capstone_Source_Capstone_CapstoneGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Capstone_Source_Capstone_CapstoneGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACapstoneGameModeBase(); \
+	friend struct Z_Construct_UClass_ACapstoneGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACapstoneGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Capstone"), NO_API) \
+	DECLARE_SERIALIZER(ACapstoneGameModeBase)
+
+
+#define Capstone_Source_Capstone_CapstoneGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACapstoneGameModeBase(); \
+	friend struct Z_Construct_UClass_ACapstoneGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACapstoneGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Capstone"), NO_API) \
+	DECLARE_SERIALIZER(ACapstoneGameModeBase)
+
+
+#define Capstone_Source_Capstone_CapstoneGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACapstoneGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACapstoneGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACapstoneGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACapstoneGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACapstoneGameModeBase(ACapstoneGameModeBase&&); \
+	NO_API ACapstoneGameModeBase(const ACapstoneGameModeBase&); \
+public:
+
+
+#define Capstone_Source_Capstone_CapstoneGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACapstoneGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACapstoneGameModeBase(ACapstoneGameModeBase&&); \
+	NO_API ACapstoneGameModeBase(const ACapstoneGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACapstoneGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACapstoneGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACapstoneGameModeBase)
+
+
+#define Capstone_Source_Capstone_CapstoneGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Capstone_Source_Capstone_CapstoneGameModeBase_h_12_PROLOG
+#define Capstone_Source_Capstone_CapstoneGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Capstone_Source_Capstone_CapstoneGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Capstone_Source_Capstone_CapstoneGameModeBase_h_15_SPARSE_DATA \
+	Capstone_Source_Capstone_CapstoneGameModeBase_h_15_RPC_WRAPPERS \
+	Capstone_Source_Capstone_CapstoneGameModeBase_h_15_INCLASS \
+	Capstone_Source_Capstone_CapstoneGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Capstone_Source_Capstone_CapstoneGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Capstone_Source_Capstone_CapstoneGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Capstone_Source_Capstone_CapstoneGameModeBase_h_15_SPARSE_DATA \
+	Capstone_Source_Capstone_CapstoneGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Capstone_Source_Capstone_CapstoneGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Capstone_Source_Capstone_CapstoneGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CAPSTONE_API UClass* StaticClass<class ACapstoneGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Capstone_Source_Capstone_CapstoneGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
